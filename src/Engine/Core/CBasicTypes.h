2f25dcc3344cc6d27be81268fbe21cf1e2ce4b2e
"Daniel Guzman" <daniel.guzman85@gmail.com>
2013-05-06 23:48:11 +0200

    CBasicTypes changes.

--- src/Engine/Core/CBasicTypes.h
+++ src/Engine/Core/CBasicTypes.h
@@ -1,26 +1,32 @@
 #ifndef _CBASIC_TYPES_H_
 #define _CBASIC_TYPES_H_
 
-#include <SDL2/SDL_stdinc.h>
+#include <cstdint>
+#include <climits>
 
-typedef int8_t			s8;
-typedef uint8_t			u8;
 
-typedef int16_t			s16;
-typedef uint16_t		u16;
+typedef std::int8_t			s8;
+typedef std::uint8_t		u8;
 
-typedef int32_t			s32;
-typedef uint32_t		u32;
+typedef std::int16_t		s16;
+typedef std::uint16_t		u16;
 
-typedef int64_t			s64;
-typedef uint64_t		u64;
+typedef std::int32_t		s32;
+typedef std::uint32_t		u32;
 
-typedef int64_t			s64;
-typedef uint64_t		u64;
+typedef std::int64_t		s64;
+typedef std::uint64_t		u64;
 
-typedef float_t 		f32;
+typedef std::int64_t		s64;
+typedef std::uint64_t		u64;
 
-typedef double_t		f64;
+static_assert(sizeof(float)*CHAR_BIT==32, "float is not 32 bit on this architecture, fix the f32 typedef.");
+typedef float 				f32;
+
+static_assert(sizeof(double)*CHAR_BIT==64, "double is not 64 bit on this architecture, fix the f64 typedef.");
+typedef double				f64;
+
+
 
 
 
